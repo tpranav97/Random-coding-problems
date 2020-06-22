cfa365812dd35ab4a9369053ed81eddc70603d54
tpranav97 <thoratpranav@hotmail.com>
2020-06-22 19:32:44 +0530

    Update NumberOfSetBitsforGivenDecimal.cpp

--- NumberOfSetBitsforGivenDecimal.cpp
+++ NumberOfSetBitsforGivenDecimal.cpp
@@ -1,10 +1,45 @@
-/******************************************************************************
+// Two methods below -
+/*
+Brian Kernighan’s Algorithm:
+   1  Initialize count: = 0
+   2  If integer n is not zero
+      (a) Do bitwise & with (n-1) and assign the value back to n
+          n: = n&(n-1)
+      (b) Increment count by 1
+      (c) go to step 2
+   3  Else return count
+   
+   Refer - gfg
+*/
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+#include <iostream> 
+using namespace std; 
+class bits
+{ 
+public: 
+    int countSetBits(int n) 
+    { 
+        int count = 0; 
+        while (n) 
+        { 
+            n &= (n - 1); 
+            count++; 
+        } 
+        return count; 
+    } 
+}; 
 
-*******************************************************************************/
+int main() 
+{ 
+    bits obj; 
+    int i ;
+    cin>>i;
+    cout << obj.countSetBits(i); 
+    return 0; 
+} 
+
+// -------------------------------------------
+// Interative menthod - convert to binary and run counter
 
 #include <iostream>
 #include <vector>
